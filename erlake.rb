ad3b456aad91254af0c78afa47d61f7fabc43f05
Jeff Hodges <jeff@somethingsimilar.com>
2008-05-12 15:50:05 -0700

    using a more specific version name just in case

--- erlake.rb
+++ erlake.rb
@@ -2,7 +2,7 @@ require 'rake'
 require 'rake/tasklib'
 
 module Erlake
-  Version = "0.1"
+  Version = "0.1.0"
   class NoProjectDirectoryGivenError < StandardError; end
   class NoProjectNameGivenError < StandardError; end
   class NotAnErlangFileError < StandardError; end
